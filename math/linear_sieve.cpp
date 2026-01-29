f54cb6b76549d7bc6e64cac455261cbea2270468
Monier Ayman <monierayman2007@gmail.com>
2026-01-29 18:55:32 +0200

    Add tests for linear sieve algorithm

--- math/linear_sieve.cpp
+++ math/linear_sieve.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <cassert>
 using namespace std;
 
 // Linear sieve: finds primes up to N
@@ -18,12 +19,33 @@ void linear_sieve(int N, vector<int> &primes, vector<int> &spf){
     }
 }
 
+// Test the linear sieve
+void tests() {
+    int N = 20;
+    vector<int> primes, spf;
+    linear_sieve(N, primes, spf);
+
+    // Expected primes up to 20
+    vector<int> expected_primes{2, 3, 5, 7, 11, 13, 17, 19};
+    assert(primes == expected_primes);
+
+    // Expected smallest prime factors up to 20
+    vector<int> expected_spf{
+        0, 0, 2, 3, 2, 5, 2, 7, 2, 3, 2, 11, 2, 13, 2, 3, 2, 17, 2, 19, 2
+    };
+    assert(spf == expected_spf);
+
+    cout << "All tests passed!" << endl;
+}
+
 int main() {
+    tests(); // run automated tests
+
     int N = 50;
     vector<int> primes, spf;
     linear_sieve(N, primes, spf);
 
-    cout<< "Primes up to " << N << ": ";
+    cout << "Primes up to " << N << ": ";
     for(int p : primes) cout << p << " ";
     cout << endl;
 
